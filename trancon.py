e8903a44ff8a8bb44ab16e4b5bd5730f9e648990
Sebastian Arcos <sebastianarcos2106@gmail.com>
2024-05-26 12:10:25 -0500

    archivos

---
+++ trancon.py
@@ -0,0 +1,18 @@
+import cv2
+
+image=cv2.imread("tranconn.webp")
+
+carros=cv2.CascadeClassifier("cars.xml")
+
+gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+cars=carros.detectMultiScale(gray,1.1,1)
+
+
+for(x,y,w,h) in cars:
+    cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+
+cv2.imshow("Autos",image)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
